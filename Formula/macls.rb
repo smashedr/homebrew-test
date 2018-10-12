2d53e24c5973c37395a76c191217640016d2da3a
Shane <shane@shanerice.me>
2018-10-11 23:05:42 -0700

    :apple:

--- Formula/macls.rb
+++ Formula/macls.rb
@@ -6,7 +6,7 @@ class Macls < Formula
   sha256 "bb38e04ca01881df5e6b92e2231f3173ee6d610b32af3068e8fe6b001c51a10f"
 
 def install
-    mv Dir.glob("macls").first
+    # mv Dir.glob("macls").first
     bin.install "macls"
   end
 
