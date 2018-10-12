ed650d08c9f4bfab2f9e85e17974dee4fb00b8e5
Shane <shane@shanerice.me>
2018-10-11 23:10:52 -0700

    :apple:

--- Formula/macls.rb
+++ Formula/macls.rb
@@ -1,12 +1,12 @@
 class Macls < Formula
   desc "My Mac LS"
   homepage "https://github.com/smashedr/homebrew-test"
-  url "https://imgg.site/f/macls"
+  url "https://imgg.site/f/macls-osx"
   version "0.0.1"
   sha256 "bb38e04ca01881df5e6b92e2231f3173ee6d610b32af3068e8fe6b001c51a10f"
 
 def install
-    # mv Dir.glob("macls").first
+    mv Dir.glob("macls-osx*").first, "macls"
     bin.install "macls"
   end
 
