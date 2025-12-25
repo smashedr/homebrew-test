48cbb3ee6de3d00efe64012c6c4ef82ac0c9a4e0
Shane <6071159+smashedr@users.noreply.github.com>
2025-12-25 14:15:16 -0800

    Brew Retarded

--- Formula/npmstat.rb
+++ Formula/npmstat.rb
@@ -4,31 +4,12 @@ class Npmstat < Formula
   license "MIT"
   version "0.1.0"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      # url "https://github.com/cssnr/npmstat/releases/latest/download/macos-arm64.zip"
-      url "https://github.com/cssnr/npmstat/releases/download/#{version}/macos-arm64.zip"
-    end
-    if Hardware::CPU.intel?
-      # url "https://github.com/cssnr/npmstat/releases/latest/download/macos-amd64.zip"
-      url "https://github.com/cssnr/npmstat/releases/download/#{version}/macos-amd64.zip"
-    end
-  end
+  url "https://github.com/cssnr/npmstat/releases/download/#{version}/npmstat-#{version}.tar.gz"
 
-  on_linux do
-    if Hardware::CPU.arm?
-      # url "https://github.com/cssnr/npmstat/releases/latest/download/linux-arm64.zip"
-      url "https://github.com/cssnr/npmstat/releases/download/#{version}/linux-arm64.zip"
-    end
-    if Hardware::CPU.intel?
-      # url "https://github.com/cssnr/npmstat/releases/latest/download/linux-amd64.zip"
-      url "https://github.com/cssnr/npmstat/releases/download/#{version}/linux-amd64.zip"
-    end
-  end
+  depends_on "python@3.y"
 
   def install
-    chmod 0755, "npmstat"
-    bin.install "npmstat"
+    virtualenv_install_with_resources
   end
 
   test do
