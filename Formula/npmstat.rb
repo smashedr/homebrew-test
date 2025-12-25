869e0a00f0a8361963e2476b1494470db30967f1
Shane <6071159+smashedr@users.noreply.github.com>
2025-12-24 18:47:20 -0800

    omfg brew is retarded

--- Formula/npmstat.rb
+++ Formula/npmstat.rb
@@ -6,23 +6,23 @@ class Npmstat < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/cssnr/npmstat/releases/latest/download/macos-arm64.zip"
-      # sha256 "586127543a6a6724e48454c47f6ed2196d5e603c4bb160489cc78315123a948a"
+      # url "https://github.com/cssnr/npmstat/releases/latest/download/macos-arm64.zip"
+      url "https://github.com/cssnr/npmstat/releases/download/#{version}/macos-arm64.zip"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/cssnr/npmstat/releases/latest/download/macos-amd64.zip"
-      # sha256 "ffbc86e31ea4eee51e3eb2289979533f627ba45c8da325c208c72a059bfdad17"
+      # url "https://github.com/cssnr/npmstat/releases/latest/download/macos-amd64.zip"
+      url "https://github.com/cssnr/npmstat/releases/download/#{version}/macos-amd64.zip"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/cssnr/npmstat/releases/latest/download/linux-arm64.zip"
-      # sha256 "586127543a6a6724e48454c47f6ed2196d5e603c4bb160489cc78315123a948a"
+      # url "https://github.com/cssnr/npmstat/releases/latest/download/linux-arm64.zip"
+      url "https://github.com/cssnr/npmstat/releases/download/#{version}/linux-arm64.zip"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/cssnr/npmstat/releases/latest/download/linux-amd64.zip"
-      # sha256 "ffbc86e31ea4eee51e3eb2289979533f627ba45c8da325c208c72a059bfdad17"
+      # url "https://github.com/cssnr/npmstat/releases/latest/download/linux-amd64.zip"
+      url "https://github.com/cssnr/npmstat/releases/download/#{version}/linux-amd64.zip"
     end
   end
 
@@ -32,7 +32,7 @@ class Npmstat < Formula
   end
 
   test do
-    system "#{bin}/npmstat", "--version"
+    # system "#{bin}/npmstat", "--version"
     assert_match "#{version}", shell_output("#{bin}/npmstat -V")
   end
 end
