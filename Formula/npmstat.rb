3ecc8d6cb7c348b7ae9e8560772ad7531d0c6aa0
Shane <6071159+smashedr@users.noreply.github.com>
2025-12-24 18:14:51 -0800

    Testing npmstat.rb - AI REALLY RETARDED

--- Formula/npmstat.rb
+++ Formula/npmstat.rb
@@ -6,33 +6,30 @@ class Npmstat < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/cssnr/npmstat/releases/latest/download/macos-arm64.zip"
+      filename "macos-arm64.zip"
       # sha256 "586127543a6a6724e48454c47f6ed2196d5e603c4bb160489cc78315123a948a"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/cssnr/npmstat/releases/latest/download/macos-amd64.zip"
+      filename "macos-amd64.zip"
       # sha256 "ffbc86e31ea4eee51e3eb2289979533f627ba45c8da325c208c72a059bfdad17"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/cssnr/npmstat/releases/latest/download/linux-arm64.zip"
+      filename "linux-arm64.zip"
       # sha256 "586127543a6a6724e48454c47f6ed2196d5e603c4bb160489cc78315123a948a"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/cssnr/npmstat/releases/latest/download/linux-amd64.zip"
+      filename "linux-amd64.zip"
       # sha256 "ffbc86e31ea4eee51e3eb2289979533f627ba45c8da325c208c72a059bfdad17"
     end
   end
 
+  url "https://github.com/cssnr/npmstat/releases/latest/download/#{filename}"
+  puts "Downloading: #{url}"
+
   def install
-    # zip_file = Dir.glob("*").first
-    # puts "RETARDS #{zip_file}"
-    # if zip_file.nil?
-    #   raise "AI IS RETARDED."
-    # end
-    # system "unzip", zip_file
     chmod 0755, "npmstat"
     bin.install "npmstat"
   end
