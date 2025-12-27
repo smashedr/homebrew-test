eb26271b1d9b5bd6d656e9efb50db048e5b75e8b
Shane <6071159+smashedr@users.noreply.github.com>
2025-12-26 22:23:44 -0800

    done

--- Formula/toml-run.rb
+++ Formula/toml-run.rb
@@ -9,10 +9,6 @@ class TomlRun < Formula
 
   depends_on "python3"
 
-  # def python3
-  #   "python3"
-  # end
-
   resource "tomli" do
     url "https://files.pythonhosted.org/packages/52/ed/3f73f72945444548f33eba9a87fc7a6e969915e7b1acc8260b30e1f76a2f/tomli-2.3.0.tar.gz"
     sha256 "64be704a875d2a59753d80ee8a533c3fe183e3f06807ff7dc2232938ccb01549"
@@ -20,7 +16,7 @@ class TomlRun < Formula
 
   def install
     venv = virtualenv_create(libexec, "python3")
-    if Formula["python"].version < Version.new("3.15")
+    if Formula["python"].version < Version.new("3.11")
       venv.pip_install resources
     end
     venv.pip_install_and_link buildpath
