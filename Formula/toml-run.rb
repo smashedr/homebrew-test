a134d2e8c7634da42898a19b0ae117bd22d5a8a9
Shane <6071159+smashedr@users.noreply.github.com>
2025-12-26 21:48:25 -0800

    AI RETARDED AI RETARDED AI RETARDED AI RETARDED

--- Formula/toml-run.rb
+++ Formula/toml-run.rb
@@ -9,15 +9,17 @@ class TomlRun < Formula
 
   depends_on "python"
 
-  if Formula["python"].version < Version.new("3.15")
-    resource "tomli" do
-      url "https://files.pythonhosted.org/packages/52/ed/3f73f72945444548f33eba9a87fc7a6e969915e7b1acc8260b30e1f76a2f/tomli-2.3.0.tar.gz"
-      sha256 "64be704a875d2a59753d80ee8a533c3fe183e3f06807ff7dc2232938ccb01549"
-    end
+  resource "tomli" do
+    url "https://files.pythonhosted.org/packages/52/ed/3f73f72945444548f33eba9a87fc7a6e969915e7b1acc8260b30e1f76a2f/tomli-2.3.0.tar.gz"
+    sha256 "64be704a875d2a59753d80ee8a533c3fe183e3f06807ff7dc2232938ccb01549"
   end
 
   def install
-    virtualenv_install_with_resources
+    if Formula["python"].version < Version.new("3.11")
+      virtualenv_install_with_resources
+    else
+      virtualenv_install_without_resources
+    end
   end
 
   test do
