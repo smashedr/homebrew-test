c10a87dd0b615cd2071bc8cae7a2004de3ffb67a
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2026-01-05 11:44:04 -0800

    Bump toml-run.rb to 0.0.32

--- Formula/toml-run.rb
+++ Formula/toml-run.rb
@@ -5,8 +5,8 @@ class TomlRun < Formula
   homepage "https://github.com/cssnr/toml-run"
   license "MIT"
 
-  url "https://example.com/toml_run-0.0.30.tar.gz"
-  sha256 "abcd123430"
+  url "https://example.com/toml_run-0.0.32.tar.gz"
+  sha256 "abcd123432"
 
   depends_on "python3"
 
