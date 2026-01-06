182db45d8359b62538cb8b589c5f26196e994a2d
Shane <6071159+smashedr@users.noreply.github.com>
2026-01-06 12:54:50 -0800

    Test

--- Formula/get-contributors.rb
+++ Formula/get-contributors.rb
@@ -3,7 +3,7 @@ class GetContributors < Formula
   homepage "https://vitepress-contributors.cssnr.com/"
   url "https://github.com/cssnr/vitepress-plugin-contributors/releases/download/0.0.5/get-contributors.js"
   sha256 "9309a179f2440f0ea576e31031e87f2c9eda26bc72f5eb5f9924463e75a06ec1"
-  license "GPL-3.0"
+  license "GPL-3.0-only"
 
   depends_on "node"
 
