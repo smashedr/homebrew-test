eee0d008b1227f8aa96ac4b5f894384e91987391
Shane <6071159+smashedr@users.noreply.github.com>
2026-01-28 14:37:13 -0800

    Update hls.rb

--- Formula/hls.rb
+++ Formula/hls.rb
@@ -8,22 +8,22 @@ class Hls < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/smashedr/hls-downloader-go/releases/latest/download/client_Darwin_x86_64.tar.gz"
+      url "https://github.com/smashedr/hls-downloader-go/releases/download/#{version}/client_Darwin_x86_64.tar.gz"
       # sha256 "56b9c057eb6eaabc15ee66fe7452698fe5640c1cf442403c3c82b43edb3fd093"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/smashedr/hls-downloader-go/releases/latest/download/client_Darwin_arm64.tar.gz"
+      url "https://github.com/smashedr/hls-downloader-go/releases/download/#{version}/client_Darwin_arm64.tar.gz"
       # sha256 "bae3d76fbf7bdc82b35c9f31eea8a3363a12d61ffa5dda4cedf5d66d86b45465"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/smashedr/hls-downloader-go/releases/latest/download/client_Linux_x86_64.tar.gz"
+      url "https://github.com/smashedr/hls-downloader-go/releases/download/#{version}/client_Linux_x86_64.tar.gz"
       # sha256 "9d52a25e599be68aa4b8a3f31d819d965fa642ae2edb3eec9e70b1da28f91442"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/smashedr/hls-downloader-go/releases/latest/download/client_Linux_arm64.tar.gz"
+      url "https://github.com/smashedr/hls-downloader-go/releases/download/#{version}/client_Linux_arm64.tar.gz"
       # sha256 "835fd5cb1f5a0222fb323094c9094ea34a7335ffdfeeeca78ac17702fffe87eb"
     end
   end
@@ -32,11 +32,9 @@ class Hls < Formula
     app_name = "org.cssnr.hls.downloader"
     description = "HLS Video Downloader"
 
-    # Install the client binary
     libexec.install "client"
     chmod 0755, libexec/"client"
 
-    # Create manifest files
     chrome_manifest = {
       "name" => app_name,
       "description" => description,
